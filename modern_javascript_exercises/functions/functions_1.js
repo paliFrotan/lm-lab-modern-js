57b5a955dea3780d95e6a58cf58ad138dcc6bb23
Pali Frotan <pali.frotan@icloud.com>
2023-05-24 11:06:01 +0100

    Feat: use arrow function

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -7,25 +7,25 @@
 
 const people = ["Harry", "Hermione", "Ron"];
 
-function logPerson(person) {
-  console.log("The person is " + person);
-}
+//function logPerson(person) {
+//  console.log("The person is " + person);
+//}
 
-people.forEach(logPerson);
+//people.forEach(logPerson);
 
 // Function in a Variable
 // Store the logPerson() function in a variable called performLogPerson
 
-// Your code here
+//performLogPerson = logPerson(person);
 
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
 
-// Your code here
+const arrowVersionOfLogPerson = person => console.log("The person is "+person); 
 
 // Anonymous Arrow Functions
 // Replace logPerson below with an Anonymous Arrow Function that does the same thing!
 
-people.forEach(logPerson);
+people.forEach(person =>console.log("The person is "+person));
 
 // In JavaScript there are lots of ways to write functions. There is no right or wrong way, but arrow functions are the most modern. 🙂
