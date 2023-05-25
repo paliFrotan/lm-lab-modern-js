4ef7f4b5ae4dd4828dab6396b8633e84cee5b7bb
Pali Frotan <pali.frotan@icloud.com>
2023-05-25 11:45:17 +0100

    Feat: destructuring_arrays function shortened

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,13 @@
 // Instructions can be found in destructuring_arrays.md
 
-export function sequence(order) {
-  const first = order[0];
-  const second = order[1];
-  const third = order[2];
+// export function sequence(order) {
+//   const first = order[0];
+//   const second = order[1];
+//   const third = order[2];
 
-  return first + ", " + second + ", " + third;
-}
+//   return first + ", " + second + ", " + third;
+// }
+//people.forEach(person =>console.log("The person is "+person));
+
+export const sequence = order => order[0] + ", " + order[1] + ", " + order[2];
+
