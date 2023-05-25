1a2bf059dab9fac5b55a5fd36fcc55dc90131c05
Pali Frotan <pali.frotan@icloud.com>
2023-05-25 17:52:30 +0100

    Fix: all tests working

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -1,13 +1,15 @@
 // Instructions can be found in imperative_vs_declarative.md
 
-export function longPasswords(passwords) {
-  let longPasswords = [];
+// export function longPasswords(passwords) {
+//   let longPasswords = [];
 
-  for (let i = 0; i < passwords.length; i++) {
-    const password = passwords[i];
-    if (password.length >= 9) {
-      longPasswords.push(password);
-    }
-  }
-  return longPasswords;
-}
+//   for (let i = 0; i < passwords.length; i++) {
+//     const password = passwords[i];
+//     if (password.length >= 9) {
+//       longPasswords.push(password);
+//     }
+//   }
+//   return longPasswords;
+// }
+
+export const longPasswords = (passwords) => passwords.filter(p => p.length >=9);
