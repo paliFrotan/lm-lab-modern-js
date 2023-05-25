dc142e9c1019be32f8868eab27a201a974e1a126
Pali Frotan <pali.frotan@icloud.com>
2023-05-25 17:40:59 +0100

    Feat: tenaries  uses tenary op instead of if

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,7 +1,9 @@
 // Instructions can be found in ternaries.md
 
-export function sayHello(name) {
-  if (name === undefined) {
-    return "Hello you!";
-  } else return "Hello, " + name + "!";
-}
+// export function sayHello(name) {
+//   if (name === undefined) {
+//     return "Hello you!";
+//   } else return "Hello, " + name + "!";
+// }
+
+export const sayHello = aname => (aname === undefined) ? "Hello you!" : "Hello, " + aname + "!";
